3d561911097248ff5210f5744ef5f11429f800fc
Patrick Steele <steele.pat@gmail.com>
2018-10-21 12:46:18 -0400

    Remove threading
    Glpk is not multithreaded, so have to be careful with threads.

--- test/ApiTests.hs
+++ test/ApiTests.hs
@@ -10,6 +10,7 @@ import Math.Programming.Glpk
 test_tree :: TestTree
 test_tree = testGroup "API tests"
   [ testCase "Set/get variable names" setGetVariableNameGlpk
+  , testCase "Set/get constraint names" setGetConstraintNameGlpk
   ]
 
 setGetVariableName :: (MonadIO m, LPMonad m a) => m ()
@@ -19,9 +20,25 @@ setGetVariableName = do
   vName <- variableName x
   liftIO $ vName @?= name
 
+setGetConstraintName :: (MonadIO m, LPMonad m a) => m ()
+setGetConstraintName = do
+  let name = "foo"
+  x <- addVariable
+  c <- addConstraint (1 *: x .>= 0) `named` name
+  cName <- constraintName c
+  liftIO $ cName @?= name
+
 setGetVariableNameGlpk :: IO ()
 setGetVariableNameGlpk = do
   result <- runGlpk setGetVariableName
+  case result of
+    Left errorMsg -> assertFailure (show errorMsg)
+    Right () -> return ()
+  return ()
+
+setGetConstraintNameGlpk :: IO ()
+setGetConstraintNameGlpk = do
+  result <- runGlpk setGetConstraintName
   case result of
     Left errorMsg -> assertFailure (show errorMsg)
     Right () -> return ()
